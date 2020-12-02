5994e60a58f79c697b787031a1d4b4246c448490
kostas <classcreator21@outlook.com>
2020-12-02 11:09:28 +0200

    made a stacked card

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,7 +1,15 @@
 import React from 'react';
+import '../Styles/Projects.css';
 
 const Projects = () => {
-  return <p>PROJECTS</p>;
+  return (
+    <div className="welcomeContainer">
+      <div className="card">
+        <h2>ANTE VRES TITLO</h2>
+        <p>welcome to my projects</p>
+      </div>
+    </div>
+  );
 };
 
 export default Projects;
