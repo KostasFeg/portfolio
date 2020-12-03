d8d40ff2cc2d4e0494b301dba9b36e680ee5e88b
kostas <classcreator21@outlook.com>
2020-12-03 12:47:55 +0200

    created contact form and connected it to Netlify forms

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,7 +1,74 @@
-import React from 'react';
-
+import React, { useState } from 'react';
+import '../Styles/Contact.css';
 const Contact = () => {
-  return <p>CONTACT</p>;
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
+
+  const encode = (data) => {
+    return Object.keys(data)
+      .map(
+        (key) => encodeURIComponent(key) + '=' + encodeURIComponent(data[key])
+      )
+      .join('&');
+  };
+
+  const handleSubmit = (e) => {
+    const objectTobeSubmitted = { name: name, email: email, message: message };
+    console.log(objectTobeSubmitted);
+
+    fetch('/', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+      body: encode({
+        'form-name': 'contact',
+        ...objectTobeSubmitted,
+      }),
+    })
+      .then(() => alert('Success'))
+      .catch((error) => alert(error));
+
+    e.preventDefault();
+  };
+
+  return (
+    <div className="welcomeContainer">
+      <div className="card">
+        <div className="cardContent">
+          <h2>Contact me</h2>
+          <form className="form" onSubmit={handleSubmit} netlify>
+            <label for="name">Name</label>
+            <input
+              placeholder="Your name"
+              type="text"
+              name="name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+            <label for="email">Email</label>
+            <input
+              placeholder="E-mail"
+              type="email"
+              name="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <label for="message">Message</label>
+            <textarea
+              placeholder="Type your message..."
+              name="message"
+              value={message}
+              onChange={(e) => setMessage(e.target.value)}
+            />
+
+            <button className="submitBtn" type="submit">
+              Send
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Contact;
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,12 +1,47 @@
 import React from 'react';
 import '../Styles/Projects.css';
+import expressjs from './Illustrations/expressjs.svg';
+import mongodb from './Illustrations/mongodb.svg';
+import reactjs from './Illustrations/react-js.svg';
+import nodejs from './Illustrations/node-js.svg';
+import materialui from './icons/materialui.svg';
+import tensorflow from './icons/tensorflow.svg';
+import dogshelter from './icons/dog-shelter.png';
+import simpleweather from './icons/simpleweather.png';
 
 const Projects = () => {
   return (
-    <div className="welcomeContainer">
-      <div className="card">
-        <h2>ANTE VRES TITLO</h2>
-        <p>welcome to my projects</p>
+    <div>
+      <div className="projectContainer">
+        <div className="projectCard">
+          <img alt="random" src={simpleweather} />
+          <h3>Simple-Weather</h3>
+          <p>
+            As the name states: a simple weather app based on the openweathermap
+            free api that covers the basics.
+          </p>
+          <div className="projectIcons">
+            <img className="card2img" alt="material ui logo" src={reactjs} />
+            <img className="card2img" alt="material ui logo" src={materialui} />
+          </div>
+        </div>
+        <div className="projectCard">
+          <img alt="random" src={dogshelter} />
+          <h3>Dog-Shelter: A better place for strays</h3>
+          <p>
+            A full-stack CRUD project: Analyze dog breeds with TensorFlowjs,
+            make your user-account, like other posts and see info about the
+            dogs.
+          </p>
+          <div className="projectIcons">
+            <img className="card2img" alt="material ui logo" src={nodejs} />
+            <img className="card2img" alt="material ui logo" src={mongodb} />
+            <img className="card2img" alt="material ui logo" src={expressjs} />
+            <img className="card2img" alt="material ui logo" src={reactjs} />
+            <img className="card2img" alt="material ui logo" src={materialui} />
+            <img className="card2img" alt="material ui logo" src={tensorflow} />
+          </div>
+        </div>
       </div>
     </div>
   );
